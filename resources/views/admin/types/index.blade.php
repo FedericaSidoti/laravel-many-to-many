98d9d6b744df6c210912a3488109ea1ca8f8c8b4
FedericaSidoti <federicasidoti21@gmail.com>
2024-01-13 15:01:41 +0100

    FIX- close butoons in modals

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -42,7 +42,7 @@
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="exampleModalLabel">Attenzione</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
                             </div>
                             <div class="modal-body">
                                 <h3>Vuoi davvero eliminare {{$type->name}}?</h3>
