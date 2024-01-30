322a9090408ba26c44819d4b1d38c6e140a0a977
FedericaSidoti <federicasidoti21@gmail.com>
2024-01-30 20:52:54 +0100

    ADD- api (lost commit....)

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $results = Project::with('type', 'technologies')->paginate(20);
+
+        return response()->json([
+            'results' => $results,
+            'success' => true
+        ]);
+    }
+
+    public function show(Project $project)
+    {
+        // $post = Post::with('category','tags')->where('slug',$slug)->first();
+        $project->load('type', 'technologies');
+
+        return response()->json([
+            'post' => $project
+        ]);
+    }
+}
